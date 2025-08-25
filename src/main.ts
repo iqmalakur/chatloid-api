962b3513d35300673b95befbdad4e48150551a10
iqmalakur <iakbarkurnia@gmail.com>
2025-08-25 21:21:35 +0700

    feat: initialize swagger api docs

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { FastifyAdapter } from '@nestjs/platform-fastify';
 import { LoggerUtil } from './utils/logger.util';
 import { PORT } from './configs/app.config';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new LoggerUtil('Main');
@@ -10,6 +11,35 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, new FastifyAdapter());
   app.enableCors();
   logger.info('Loaded app modules');
+
+  const swaggerConfig: Omit<OpenAPIObject, 'paths'> = new DocumentBuilder()
+    .setTitle('ChatLoid API')
+    .setDescription('API documentation for ChatLoid Application')
+    .setVersion('1.0.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'jwt',
+    )
+    .addTag('Auth')
+    .addTag('Users')
+    .addTag('Contacts')
+    .addTag('Contact Requests')
+    .addTag('Chats')
+    .build();
+
+  const swaggerDocument: OpenAPIObject = SwaggerModule.createDocument(
+    app,
+    swaggerConfig,
+  );
+  SwaggerModule.setup('api', app, swaggerDocument);
+  logger.info('Generated Swagger API Documentation');
+  logger.info('Access /api to see the API Documentation');
+  logger.info('Access /api-json to see the Open API json file');
+  logger.info('Access /api-yaml to see the Open API yaml file');
 
   await app.listen(PORT, '0.0.0.0');
   logger.info(`Application started on port ${PORT}`);
