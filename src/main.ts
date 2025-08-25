d072376dd0148269f0a8af2ca607d2bbed593f1f
iqmalakur <iakbarkurnia@gmail.com>
2025-08-25 16:16:43 +0700

    feat: create app config

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter } from '@nestjs/platform-fastify';
 import { LoggerUtil } from './utils/logger.util';
+import { PORT } from './configs/app.config';
 
 async function bootstrap() {
+  const logger = new LoggerUtil('Main');
+
   const app = await NestFactory.create(AppModule, new FastifyAdapter());
-  await app.listen(process.env.PORT ?? 3000);
-  LoggerUtil.getGlobalInstance().info('Server run on port 3000');
+
+  await app.listen(PORT, '0.0.0.0');
+  logger.info(`Application started on port ${PORT}`);
 }
 
 bootstrap();
