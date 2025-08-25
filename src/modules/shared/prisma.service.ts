18d3a0e27e55eabc2add6180d9f5ff2547b1b64d
iqmalakur <iakbarkurnia@gmail.com>
2025-08-25 21:48:47 +0700

    feat: create prisma service

---
+++ src/modules/shared/prisma.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from 'generated/prisma/client';
+import { LoggerUtil } from 'src/utils/logger.util';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  private readonly logger: LoggerUtil;
+
+  public constructor() {
+    super();
+    this.logger = new LoggerUtil(this.constructor.name);
+  }
+
+  public async onModuleInit() {
+    await this.$connect();
+    this.logger.info('Database is connected');
+  }
+
+  public async onModuleDestroy() {
+    await this.$disconnect();
+    this.logger.info('Database is disconnected');
+  }
+}
