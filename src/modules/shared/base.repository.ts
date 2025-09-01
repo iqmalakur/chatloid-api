dd35b70bbbc056e15caf8a905e85b91699123065
iqmalakur <iakbarkurnia@gmail.com>
2025-09-01 20:46:47 +0700

    feat: remove injectable on BaseRepository

--- src/modules/shared/base.repository.ts
+++ src/modules/shared/base.repository.ts
@@ -1,8 +1,6 @@
 import { LoggerUtil } from 'src/utils/logger.util';
 import { PrismaService } from './prisma.service';
-import { Injectable } from '@nestjs/common';
 
-@Injectable()
 export abstract class BaseRepository {
   protected readonly logger: LoggerUtil;
   public constructor(protected readonly prisma: PrismaService) {
