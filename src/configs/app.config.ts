6cf2fcaf3025742a16b9283ff8040117478ef2b8
iqmalakur <iakbarkurnia@gmail.com>
2025-09-02 08:28:30 +0700

    feat: initialize google oauth 2.0

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -3,3 +3,4 @@ import { config } from 'dotenv';
 config();
 
 export const PORT = parseInt(process.env.PORT || '3000');
+export const BASE_URL = process.env.BASE_URL || 'http://localhost:3000';
---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { BaseService } from '../shared/base.service';
+import { AuthRepository } from './auth.repository';
+import { google } from 'googleapis';
+import {
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+} from 'src/configs/google.config';
+import { BASE_URL } from 'src/configs/app.config';
+
+@Injectable()
+export class AuthService extends BaseService {
+  private readonly oauth2Client = new google.auth.OAuth2(
+    GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET,
+    `${BASE_URL}/auth/google/callback`,
+  );
+
+  private readonly authorizedScopes = [
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/userinfo.profile',
+  ];
+
+  private readonly authorizationUrl = this.oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: this.authorizedScopes,
+    include_granted_scopes: true,
+  });
+
+  public constructor(private readonly authRepository: AuthRepository) {
+    super();
+  }
+}
