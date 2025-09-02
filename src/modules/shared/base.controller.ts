a99079c2227385656600073002194f28f401249f
iqmalakur <iakbarkurnia@gmail.com>
2025-09-02 11:01:36 +0700

    feat: create base controller

---
+++ src/modules/shared/base.controller.ts
@@ -0,0 +1,8 @@
+import { LoggerUtil } from 'src/utils/logger.util';
+
+export abstract class BaseController {
+  protected readonly logger: LoggerUtil;
+  public constructor() {
+    this.logger = new LoggerUtil(this.constructor.name);
+  }
+}
