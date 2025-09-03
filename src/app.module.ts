7d1371b8a8ea133033e07175743f866114e48182
iqmalakur <iakbarkurnia@gmail.com>
2025-09-03 16:37:19 +0700

    feat: remove default app controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EventModule } from './modules/event/event.module';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [AuthModule, EventModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
