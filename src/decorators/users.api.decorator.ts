1f023da1a7177b95bf5daf7891d345d479159d7a
iqmalakur <iakbarkurnia@gmail.com>
2025-09-04 11:18:54 +0700

    feat: add swagger docs for user info

---
+++ src/decorators/users.api.decorator.ts
@@ -0,0 +1,27 @@
+import { applyDecorators, HttpStatus } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { UserInfoResDto } from 'src/modules/users/users.dto';
+import {
+  ApiBadRequest,
+  ApiNotFound,
+  ApiServerError,
+  ApiUnauthorized,
+} from './api-response.decorator';
+
+export const ApiUserInfo = (): MethodDecorator => {
+  return applyDecorators(
+    ApiOperation({
+      summary: 'Get authenticated user info',
+      description: 'Get authenticated user info from JWT',
+    }),
+    ApiResponse({
+      status: HttpStatus.OK,
+      description: 'Success get user info',
+      type: UserInfoResDto,
+    }),
+    ApiBadRequest('Token is not provided', 'Token is not provided'),
+    ApiUnauthorized('Token is not valid', 'Token is not valid'),
+    ApiNotFound('User is not found', 'User is not found'),
+    ApiServerError(),
+  );
+};
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,18 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { BaseController } from '../shared/base.controller';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiSecurity, ApiTags } from '@nestjs/swagger';
 import { UsersService } from './users.service';
 import { CurrentUserId } from 'src/decorators/current-user-id.decorator';
 import { UserInfoResDto } from './users.dto';
+import { ApiUserInfo } from 'src/decorators/users.api.decorator';
 
 @Controller('users')
 @ApiTags('Users')
+@ApiSecurity('jwt')
 export class UsersController extends BaseController {
   public constructor(private readonly service: UsersService) {
     super();
   }
 
   @Get('me')
+  @ApiUserInfo()
   public async UserInfo(
     @CurrentUserId() userId: string,
   ): Promise<UserInfoResDto> {
