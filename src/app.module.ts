28c4cd06e89f828cd4bbf66bbabc2ff0f003adad
iqmalakur <iakbarkurnia@gmail.com>
2025-09-04 11:18:26 +0700

    fix: fix routes that use token middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,15 +8,13 @@ import { EventModule } from './modules/event/event.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { TokenMiddleware } from './middlewares/token.middleware';
+import { UsersController } from './modules/users/users.controller';
 
 @Module({
   imports: [AuthModule, EventModule, UsersModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(TokenMiddleware)
-      .exclude({ path: 'auth/(.*)', method: RequestMethod.ALL })
-      .forRoutes('*');
+    consumer.apply(TokenMiddleware).forRoutes(UsersController);
   }
 }
