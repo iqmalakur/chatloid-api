c09889cd5fa5bc72ec64ebaec6fac9eaa677b7cf
iqmalakur <iakbarkurnia@gmail.com>
2025-09-07 11:11:42 +0700

    feat: create e2e test for create chat room

--- test/chats.e2e-spec.ts
+++ test/chats.e2e-spec.ts
@@ -15,7 +15,6 @@ describe('ChatsController (e2e)', () => {
 
   let ucup: any;
   let otong: any;
-  let chatRoom: any;
 
   beforeAll(async () => {
     prisma = new PrismaClient();
@@ -57,26 +56,9 @@ describe('ChatsController (e2e)', () => {
         picture: 'http://example.com/otong.png',
       },
     });
-
-    chatRoom = await prisma.chatRoom.create({
-      data: {
-        user1Id: ucup.id,
-        user2Id: otong.id,
-      },
-    });
-
-    await prisma.message.create({
-      data: {
-        chatRoomId: chatRoom.id,
-        senderId: ucup.id,
-        content: 'Hello Otong!',
-      },
-    });
   });
 
   afterAll(async () => {
-    await prisma.message.deleteMany({ where: { chatRoomId: chatRoom.id } });
-    await prisma.chatRoom.deleteMany({ where: { id: chatRoom.id } });
     await prisma.user.deleteMany({
       where: { id: { in: [ucup.id, otong.id] } },
     });
@@ -85,30 +67,114 @@ describe('ChatsController (e2e)', () => {
     await prisma.$disconnect();
   });
 
-  it('/chats (GET) - should return chat rooms for ucup', async () => {
-    const res = await request(app.getHttpServer())
-      .get('/chats')
-      .set('Authorization', `Bearer ${makeJwt(ucup.id)}`)
-      .expect(200);
+  describe('/chats (GET)', () => {
+    let chatRoom: any;
 
-    expect(res.body).toHaveLength(1);
+    beforeAll(async () => {
+      chatRoom = await prisma.chatRoom.create({
+        data: {
+          user1Id: ucup.id,
+          user2Id: otong.id,
+        },
+      });
 
-    const room = res.body[0];
+      await prisma.message.create({
+        data: {
+          chatRoomId: chatRoom.id,
+          senderId: ucup.id,
+          content: 'Hello Otong!',
+        },
+      });
+    });
 
-    expect(room.id).toBe(chatRoom.id);
-    expect(room.picture).toBe(otong.picture);
-    expect(room.displayName).toBe(otong.name);
-    expect(room.lastMessage.content).toBe('Hello Otong!');
-    expect(new Date(room.lastMessage.createdAt)).toBeInstanceOf(Date);
+    afterAll(async () => {
+      await prisma.message.deleteMany({ where: { chatRoomId: chatRoom.id } });
+      await prisma.chatRoom.deleteMany({ where: { id: chatRoom.id } });
+    });
+
+    it('should return chat rooms for ucup', async () => {
+      const res = await request(app.getHttpServer())
+        .get('/chats')
+        .set('Authorization', `Bearer ${makeJwt(ucup.id)}`)
+        .expect(200);
+
+      expect(res.body).toHaveLength(1);
+
+      const room = res.body[0];
+
+      expect(room.id).toBe(chatRoom.id);
+      expect(room.picture).toBe(otong.picture);
+      expect(room.displayName).toBe(otong.name);
+      expect(room.lastMessage.content).toBe('Hello Otong!');
+      expect(new Date(room.lastMessage.createdAt)).toBeInstanceOf(Date);
+    });
+
+    it('should return empty if not match keyword', async () => {
+      const res = await request(app.getHttpServer())
+        .get('/chats')
+        .set('Authorization', `Bearer ${makeJwt(ucup.id)}`)
+        .query({ search: 'Charlie' })
+        .expect(200);
+
+      expect(res.body).toEqual([]);
+    });
   });
 
-  it('/chats (GET) - should return empty if not match keyword', async () => {
-    const res = await request(app.getHttpServer())
-      .get('/chats')
-      .set('Authorization', `Bearer ${makeJwt(ucup.id)}`)
-      .query({ search: 'Charlie' })
-      .expect(200);
+  describe('/chats (POST)', () => {
+    it('should create a new chat room (201)', async () => {
+      const res = await request(app.getHttpServer())
+        .post('/chats')
+        .set('Authorization', `Bearer ${makeJwt(ucup.id)}`)
+        .send({ contactId: otong.id })
+        .expect(201);
 
-    expect(res.body).toEqual([]);
+      await prisma.chatRoom.deleteMany({
+        where: { id: res.body.id },
+      });
+
+      expect(res.body).toHaveProperty('id');
+      expect(res.body.displayName).toBe(otong.name);
+      expect(res.body.picture).toBe(otong.picture);
+      expect(res.body.lastMessage).toBeNull();
+    });
+
+    it('should return existing chat room (200)', async () => {
+      const chatRoom = await prisma.chatRoom.create({
+        data: {
+          user1Id: ucup.id,
+          user2Id: otong.id,
+        },
+      });
+
+      await prisma.message.create({
+        data: {
+          chatRoomId: chatRoom.id,
+          senderId: ucup.id,
+          content: 'Hello Otong!',
+        },
+      });
+
+      const res = await request(app.getHttpServer())
+        .post('/chats')
+        .set('Authorization', `Bearer ${makeJwt(ucup.id)}`)
+        .send({ contactId: otong.id })
+        .expect(200);
+
+      await prisma.message.deleteMany({ where: { chatRoomId: chatRoom.id } });
+      await prisma.chatRoom.deleteMany({ where: { id: chatRoom.id } });
+
+      expect(res.body.id).toBe(chatRoom.id);
+      expect(res.body.displayName).toBe(otong.name);
+      expect(res.body.picture).toBe(otong.picture);
+      expect(res.body.lastMessage.content).toBe('Hello Otong!');
+    });
+
+    it('should throw NotFoundException if contact does not exist', async () => {
+      await request(app.getHttpServer())
+        .post('/chats')
+        .set('Authorization', `Bearer ${makeJwt(ucup.id)}`)
+        .send({ contactId: '550e8400-e29b-41d4-a716-446655440000' })
+        .expect(404);
+    });
   });
 });
