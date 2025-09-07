eeb635a48f27a237b69564f0bc68522d2bd40157
iqmalakur <iakbarkurnia@gmail.com>
2025-09-07 11:16:06 +0700

    fix: update api decorator for user info update

--- src/decorators/users.api.decorator.ts
+++ src/decorators/users.api.decorator.ts
@@ -3,6 +3,7 @@ import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { UserInfoResDto } from 'src/modules/users/users.dto';
 import {
   ApiBadRequest,
+  ApiConflict,
   ApiNotFound,
   ApiServerError,
   ApiUnauthorized,
@@ -29,17 +30,18 @@ export const ApiUserInfo = (): MethodDecorator => {
 export const ApiUserUpdate = (): MethodDecorator => {
   return applyDecorators(
     ApiOperation({
-      summary: 'Get authenticated user info',
-      description: 'Get authenticated user info from JWT',
+      summary: 'Update user info',
+      description: 'Update authenticated user info',
     }),
     ApiResponse({
       status: HttpStatus.OK,
-      description: 'Success get user info',
+      description: 'Success update user info',
       type: UserInfoResDto,
     }),
     ApiBadRequest('Token is not provided', 'Token is not provided'),
     ApiUnauthorized('Token is not valid', 'Token is not valid'),
     ApiNotFound('User is not found', 'User is not found'),
+    ApiConflict('Username is not available', 'Username is not available'),
     ApiServerError(),
   );
 };
